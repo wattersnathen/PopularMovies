4cf87253973c38c354e539cfdaabf825a2e0be96
Nathen Watters <wattersnathen@gmail.com>
2015-08-23 00:22:17 -0600

    Updated MainActivity to include a FragmentManager

--- app/src/main/java/com/wattersnathen/popularmovies/MainActivity.java
+++ app/src/main/java/com/wattersnathen/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wattersnathen.popularmovies;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,10 +8,23 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager mFragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+
+        if (savedInstanceState == null) {
+
+            MainActivityFragment fragment = new MainActivityFragment();
+
+            mFragmentManager.beginTransaction()
+                    .add(R.id.fragment, fragment)
+                    .commit();
+        }
     }
 
 
