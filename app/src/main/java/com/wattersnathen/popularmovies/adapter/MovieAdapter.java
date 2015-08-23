38b8e25d31127692fc6996fcf7240dfa9249bf0d
Nathen Watters <wattersnathen@gmail.com>
2015-08-23 00:15:22 -0600

    Adding Picasso

--- app/src/main/java/com/wattersnathen/popularmovies/adapter/MovieAdapter.java
+++ app/src/main/java/com/wattersnathen/popularmovies/adapter/MovieAdapter.java
@@ -4,4 +4,10 @@ package com.wattersnathen.popularmovies.adapter;
  * Created by Nathen on 8/22/2015.
  */
 public class MovieAdapter {
+
+        Picasso.with(mContext)
+                .load(imageUrl)
+                .error(R.mipmap.ic_launcher)
+                .into(holder.mImageView);
+
 }
