ab76205e1f1b697aecaf2829b913bcf9a6578f52
Nathen Watters <wattersnathen@gmail.com>
2015-08-23 00:37:50 -0600

    Helper method for building list of movies from JSON string

--- app/src/main/java/com/wattersnathen/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/wattersnathen/popularmovies/MainActivityFragment.java
@@ -23,5 +23,34 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+        private List<Movie> getMovieDataFromJson(String movieJsonString) throws JSONException {
+
+            JSONObject resultString = new JSONObject(movieJsonString);
+            JSONArray results = resultString.getJSONArray(MovieDBOperations.RESULTS);
+
+            List<Movie> movies = new ArrayList<>();
+            for (int index = 0; index < results.length(); index++) {
+                Movie movie = new Movie();
+                JSONObject movieObject = results.getJSONObject(index);
+
+                movie.setAdult(movieObject.getBoolean(MovieDBOperations.ADULT));
+                movie.setBackdropPath(movieObject.getString(MovieDBOperations.BACKDROP_PATH));
+                movie.setId(movieObject.getLong(MovieDBOperations.ID));
+                movie.setOriginalLanguage(movieObject.getString(MovieDBOperations.ORIGINAL_LANGUAGE));
+                movie.setOriginalTitle(movieObject.getString(MovieDBOperations.ORIGINAL_TITLE));
+                movie.setOverview(movieObject.getString(MovieDBOperations.OVERVIEW));
+                movie.setReleaseDate(movieObject.getString(MovieDBOperations.RELEASE_DATE));
+                movie.setPosterPath(movieObject.getString(MovieDBOperations.POSTER_PATH));
+                movie.setPopularity(movieObject.getDouble(MovieDBOperations.POPULARITY));
+                movie.setTitle(movieObject.getString(MovieDBOperations.TITLE));
+                movie.setVideo(movieObject.getBoolean(MovieDBOperations.VIDEO));
+                movie.setVoteAverage(movieObject.getDouble(MovieDBOperations.VOTE_AVERAGE));
+                movie.setVoteCount(movieObject.getInt(MovieDBOperations.VOTE_COUNT));
+
+                movies.add(movie);
+            }
+
+            return movies;
+        }
     }
 }
