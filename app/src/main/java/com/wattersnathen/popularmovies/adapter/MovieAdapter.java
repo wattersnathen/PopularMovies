46783e3b82548007570f3cd98407cbabb7e9c86a
Nathen Watters <wattersnathen@gmail.com>
2015-08-23 00:27:23 -0600

    Creating custom BaseAdapter to use with the GridView

--- app/src/main/java/com/wattersnathen/popularmovies/adapter/MovieAdapter.java
+++ app/src/main/java/com/wattersnathen/popularmovies/adapter/MovieAdapter.java
@@ -1,9 +1,46 @@
 package com.wattersnathen.popularmovies.adapter;
 
+import android.content.Context;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Nathen on 8/22/2015.
  */
-public class MovieAdapter {
+public class MovieAdapter extends BaseAdapter {
+
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+    private Context mContext;
+    private List<Movie> mMovieList;
+    private LayoutInflater inflater;
+
+    public MovieAdapter(Context context) {
+        this(context, new ArrayList<Movie>());
+    }
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        mContext = context;
+        mMovieList = movies;
+        inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return (mMovieList == null) ? 0 : mMovieList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return (mMovieList == null) ? null : mMovieList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return Long.valueOf(mMovieList.get(position).getId().toString());
+    }
 
         Picasso.with(mContext)
                 .load(imageUrl)
