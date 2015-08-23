6c0bf917b202cf7766e591e58af32e1d07783c9c
Nathen Watters <wattersnathen@gmail.com>
2015-08-23 00:21:04 -0600

    Getters and Settings for Movie member objects

--- app/src/main/java/com/wattersnathen/popularmovies/model/Movie.java
+++ app/src/main/java/com/wattersnathen/popularmovies/model/Movie.java
@@ -4,4 +4,130 @@ package com.wattersnathen.popularmovies.model;
  * Created by Nathen on 8/22/2015.
  */
 public class Movie {
+    private String mTitle;
+    private Boolean mAdult;
+    private String mBackdropPath;
+    private List<Integer> mGenreIds;
+    private Long mId;
+    private String mOriginalLanguage;
+    private String mOriginalTitle;
+    private String mOverview;
+    private String mReleaseDate;
+    private String mPosterPath;
+    private Double mPopularity;
+    private Boolean mVideo;
+    private Double mVoteAverage;
+    private Integer mVoteCount;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Boolean getAdult() {
+        return mAdult;
+    }
+
+    public void setAdult(Boolean adult) {
+        mAdult = adult;
+    }
+
+    public String getBackdropPath() {
+        return mBackdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        mBackdropPath = backdropPath;
+    }
+
+    public List<Integer> getGenreIds() {
+        return mGenreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        mGenreIds = genreIds;
+    }
+
+    public Long getId() {
+        return mId;
+    }
+
+    public void setId(Long id) {
+        mId = id;
+    }
+
+    public String getOriginalLanguage() {
+        return mOriginalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        mOriginalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return mOriginalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        mOriginalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public void setOverview(String overview) {
+        mOverview = overview;
+    }
+
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        mReleaseDate = releaseDate;
+    }
+
+    public String getPosterPath() {
+        return mPosterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        mPosterPath = posterPath;
+    }
+
+    public Double getPopularity() {
+        return mPopularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        mPopularity = popularity;
+    }
+
+    public Boolean getVideo() {
+        return mVideo;
+    }
+
+    public void setVideo(Boolean video) {
+        mVideo = video;
+    }
+
+    public Double getVoteAverage() {
+        return mVoteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        mVoteAverage = voteAverage;
+    }
+
+    public Integer getVoteCount() {
+        return mVoteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        mVoteCount = voteCount;
+    }
 }
